7147ad763f0f6ec3dc2e670f66b42b3df4ee9d2c
YashPro8158 <yashbhola120@gmail.com>
2024-01-06 21:02:57 +0530

    initial commit

---
+++ Myfunction.js
@@ -0,0 +1,10 @@
+function actvsrch() {
+  let searchactive = document.getElementById("deactive-search");
+  searchactive.classList.toggle("active-search");
+}
+// searchactive.classList.toggle('active-search');
+
+function closemenu() {
+  let close = document.getElementById('menuclose');
+  close.classList.toggle('MenuOpen');
+}
